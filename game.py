86711ecfa5fd2d8e4e257fc02c052dad5075921f
petersimpsn <157054511+petersimpsn@users.noreply.github.com>
2024-04-12 12:45:00 -0500

    project checkpoint/proposal 1

--- game.py
+++ game.py
@@ -10,47 +10,62 @@ game_board = { # DO NOT CHANGE
     "9" : "9"
 }
 
-def check_is_xo(stringin): # IN USE
-    stringin = stringin.lower()
-    if stringin == "x" or stringin == "o":
+def check_is_xo(stringin): # CHECKS IF X OR O
+    stringin = stringin.upper()
+    if stringin == "X" or stringin == "O":
         pass
     else:
-        while stringin != "x" and stringin != "o":
+        while stringin != "X" and stringin != "O":
             stringin = input("Please enter an X or an O")
+            stringin = stringin.upper()
     return stringin
 
-def check_is_x(stringin): #NOT IN USE YET (MADE FOR DOING TURNS)
-    stringin = stringin.lower()
-    if stringin == "x":
+def check_is_x(stringin): #CHECKS IF X ON X'S TURN
+    stringin = stringin.upper()
+    if stringin == "X":
         pass
     else:
-        while stringin != "x":
+        while stringin != "X":
             stringin = input("It is X's turn")
+            stringin = stringin.upper()
     return stringin
 
-def check_is_o(stringin): #NOT IN USE YET (MADE FOR DOING TURNS)
-    stringin = stringin.lower()
-    if stringin == "o":
+def check_is_o(stringin): #CHECKS IF O ON O'S TURN
+    stringin = stringin.upper()
+    if stringin == "O":
         pass
     else:
-        while stringin != "o":
+        while stringin != "O":
             stringin = input("It is O's turn")
+            stringin = stringin.upper()
     return stringin
 
-def check_whoturn(stringin, turncount): #CHECK IF IT'S O OR X, NOT IN USE YET
+def check_whoturn(stringin, turncount): #CHECK IF IT'S O'S OR X'S TURN
     if turncount % 2 == 0:
         check_is_x(stringin)
     else:
         check_is_o(stringin)
 
-def gameplay(dict): #HOW THE GAME FUNCTIONS
+def is_space_taken(dict, ask1): # CHECKS IF THE SPACE IS ALREADY TAKEN
+    while dict[ask1] == "X" or dict[ask1] == "O":
+        ask1 = input("Please select a different space: ")
+    return ask1
+
+def gameplay(dict, turncount): #HOW THE GAME FUNCTIONS
     print(dict["1"])
+    if turncount % 2 == 0:
+        ask2 = input("X or O?")
+        ask2 = check_is_x(ask2)
+    elif turncount % 2 == 1:
+        ask2 = input("X or O?")
+        ask2 = check_is_o(ask2)
     ask1 = input("Which space do you want to input for? (1-9)")
-    ask2 = input("What would you like to make that space?")
-    ask2 = check_is_xo(ask2)
+    ask1 = is_space_taken(game_board, ask1)
     dict[ask1] = ask2
     print(gameboard(dict))
-    pass
+    turncount = turncount + 1
+    print(turncount)
+    return turncount
 
 def gameboard(game_board): #DO NOT CHANGE
     print (" ", game_board["1"], " ", "|", " ", game_board["2"], " ", "|", " ", game_board["3"], " ",)
@@ -58,5 +73,73 @@ def gameboard(game_board): #DO NOT CHANGE
     print (" ", game_board["7"], " ", "|", " ", game_board["8"], " ", "|", " ", game_board["9"], " ",)
     pass
 
-while game_board["1"] == "1" and game_board["2"] == "2" and game_board["3"] == "3": #FIGURE THIS OUT
-    gameplay(game_board)
+turncount = 0
+print(gameboard(game_board))
+win = ""
+for i in range(9):
+    turncount = gameplay(game_board, turncount)
+    if game_board["1"] == "X" and game_board["2"] == "X" and game_board["3"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["4"] == "X" and game_board["5"] == "X" and game_board["6"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["7"] == "X" and game_board["8"] == "X" and game_board["9"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["1"] == "X" and game_board["4"] == "X" and game_board["7"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["2"] == "X" and game_board["5"] == "X" and game_board["8"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["3"] == "X" and game_board["6"] == "X" and game_board["9"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["1"] == "X" and game_board["5"] == "X" and game_board["9"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["7"] == "X" and game_board["5"] == "X" and game_board["3"] == "X":
+        print("X wins!")
+        break
+    elif game_board["1"] == "O" and game_board["2"] == "O" and game_board["3"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["4"] == "O" and game_board["5"] == "O" and game_board["6"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["7"] == "O" and game_board["8"] == "O" and game_board["9"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["1"] == "O" and game_board["4"] == "O" and game_board["7"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["2"] == "O" and game_board["5"] == "O" and game_board["8"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["3"] == "O" and game_board["6"] == "O" and game_board["9"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["1"] == "O" and game_board["5"] == "O" and game_board["9"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["7"] == "O" and game_board["5"] == "O" and game_board["3"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+if win != "yes":
+    print("It's a tie!")
