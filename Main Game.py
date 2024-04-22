adf6423175c6aa5b0f440eb208ab823299d2769c
petersimpsn <157054511+petersimpsn@users.noreply.github.com>
2024-04-22 13:16:07 -0500

    New stuff

--- Main Game.py
+++ Main Game.py
@@ -10,17 +10,99 @@ game_board = { # DO NOT CHANGE
     "9" : "9"
 }
 
+def check_is_xo(stringin): # CHECKS IF X OR O
+    stringin = stringin.upper()
+    if stringin == "X" or stringin == "O":
+        pass
+    else:
+        while stringin != "X" and stringin != "O":
+            stringin = input("Please enter an X or an O")
+            stringin = stringin.upper()
+    return stringin
+
+def check_is_x(stringin): #CHECKS IF X ON X'S TURN
+    stringin = stringin.upper()
+    if stringin == "X":
+        pass
+    else:
+        while stringin != "X":
+            stringin = input("It is X's turn")
+            stringin = stringin.upper()
+    return stringin
+
+def check_is_o(stringin): #CHECKS IF O ON O'S TURN
+    stringin = stringin.upper()
+    if stringin == "O":
+        pass
+    else:
+        while stringin != "O":
+            stringin = input("It is O's turn")
+            stringin = stringin.upper()
+    return stringin
+
+def check_whoturn(stringin, turncount): #CHECK IF IT'S O'S OR X'S TURN
+    if turncount % 2 == 0:
+        check_is_x(stringin)
+    else:
+        check_is_o(stringin)
+
+def is_space_taken(dict, ask1): # CHECKS IF THE SPACE IS ALREADY TAKEN
+    while dict[ask1] == "X" or dict[ask1] == "O":
+        ask1 = input("Please select a different space: ")
+    return ask1
 
 
-#Will's thoughts---right now this is just what the bot should do and does not include player input.
-#I think after each if/else statement we will need to add a way to it to go to the next section
-#of code, but I'm not sure how to do that. Right now with just this, I think it will look back to
-#other states which would completely destroy the game. Right now, if there isn't a legal move somehow,
-#it breaks (to add the else to the elif statements), which shouldn't ever happen. If there is something
-#format wise that needs to change that I messed up, tell me and I'll go back.
 
-#Idea for making it more complex: could use random number generation to choose between equally optimal
-#moves which wouldn't be much effort, but lets get the baqse game working first.
+
+
+
+
+def gameplayprevious(dict, turncount): #HOW THE GAME FUNCTIONS
+    if turncount % 2 == 0:
+        ask2 = input("X or O?")
+        ask2 = check_is_x(ask2)
+    elif turncount % 2 == 1:
+        ask2 = input("X or O?")
+        ask2 = check_is_o(ask2)
+    ask1 = input("Which space do you want to input for? (1-9)")
+    ask1 = is_space_taken(game_board, ask1)
+    dict[ask1] = ask2
+    print(gameboard(dict))
+    turncount = turncount + 1
+    print(turncount)
+    return turncount
+
+def gameplay(dict, turncount): #HOW THE GAME FUNCTIONS
+    if turncount % 2 == 0:
+        ask1 = input("Which space do you want to input for? (1-9)")
+        ask1 = is_space_taken(game_board, ask1)
+        dict[ask1] = "X"
+    elif turncount % 2 == 1:
+        bot_hard_code(dict, turncount)
+    #print(gameboard(dict))
+    turncount = turncount + 1
+    print(turncount)
+    return turncount
+
+
+
+
+
+
+
+
+
+
+
+def gameboard(game_board): #DO NOT CHANGE
+    print (" ", game_board["1"], " ", "|", " ", game_board["2"], " ", "|", " ", game_board["3"], " ",)
+    print("------|-------|------")
+    print (" ", game_board["4"], " ", "|", " ", game_board["5"], " ", "|", " ", game_board["6"], " ",)
+    print("------|-------|------")
+    print (" ", game_board["7"], " ", "|", " ", game_board["8"], " ", "|", " ", game_board["9"], " ",)
+    pass
+
+
 
 def bot_hard_code(dict, turncount):
     if game_board["5"] == "X":
@@ -36,20 +118,20 @@ def bot_hard_code(dict, turncount):
     elif game_board["2"] == "X":
         top_middle_start(dict, turncount)
 
-def is_space_taken(dict, ask1): # CHECKS IF THE SPACE IS ALREADY TAKEN
+
+def is_space_taken(dict, ask1):  # CHECKS IF THE SPACE IS ALREADY TAKEN
     while dict[ask1] == "X" or dict[ask1] == "O":
         ask1 = input("Please select a different space: ")
     return ask1
 
 
-
 def middle_start(dict, turncount):
-    dict[1] = "O"
+    dict["1"] = "O"
     print(gameboard(dict))
     turncount = turncount + 1
     ask1 = input("Which space do you want to input for? (1-9)")
     ask1 = is_space_taken(game_board, ask1)
-    dict[ask1] = ask2
+    dict[ask1] = "X"
     if game_board["4"] == "X":
         dict["6"] = "O"
         print(gameboard(dict))
@@ -87,11 +169,15 @@ def middle_start(dict, turncount):
         return turncount
     else:
         print("You got an error in middle_start")
-def top_right_start(dict, turncount):
+
+
+def top_left_start(dict, turncount):
     dict["5"] = "O"
     print(gameboard(dict))
     turncount = turncount + 1
-    # PLAYER MOVE HERE
+    ask1 = input("Which space do you want to input for? (1-9)")
+    ask1 = is_space_taken(game_board, ask1)
+    dict[ask1] = "X"
     if dict["4"] == "X":
         dict["7"] = "O"
         print(gameboard(dict))
@@ -128,14 +214,16 @@ def top_right_start(dict, turncount):
         turncount = turncount + 1
         return turncount
     else:
-        print("you got an error in top_right_start")
+        print("you got an error in top_left_start")
 
 
-def top_left_start(dict, turncount):
+def top_right_start(dict, turncount):
     dict["5"] = "O"
     print(gameboard(dict))
     turncount = turncount + 1
-    # PLAYER MOVE HERE
+    ask1 = input("Which space do you want to input for? (1-9)")
+    ask1 = is_space_taken(game_board, ask1)
+    dict[ask1] = "X"
     if dict["4"] == "X":
         dict["9"] = "O"
         print(gameboard(dict))
@@ -172,13 +260,16 @@ def top_left_start(dict, turncount):
         turncount = turncount + 1
         return turncount
     else:
-        print("You got an error in top_left_start")
+        print("You got an error in top_right_start")
+
 
 def bottom_left_start(dict, turncount):
     dict["5"] = "O"
     print(gameboard(dict))
     turncount = turncount + 1
-    #PLAYER MOVE HERE
+    ask1 = input("Which space do you want to input for? (1-9)")
+    ask1 = is_space_taken(game_board, ask1)
+    dict[ask1] = "X"
     if dict["1"] == "X":
         dict["4"] = "O"
         print(gameboard(dict))
@@ -215,11 +306,14 @@ def bottom_left_start(dict, turncount):
         turncount = turncount + 1
         return turncount
 
+
 def bottom_right_start(dict, turncount):
     dict["5"] = "O"
     print(gameboard(dict))
     turncount = turncount + 1
-    #PLAYER MOVE HERE
+    ask1 = input("Which space do you want to input for? (1-9)")
+    ask1 = is_space_taken(game_board, ask1)
+    dict[ask1] = "X"
     if dict["1"] == "X":
         dict["7"] = "O"
         print(gameboard(dict))
@@ -261,7 +355,9 @@ def top_middle_start(dict, turncount):
     dict["5"] = "O"
     print(gameboard(dict))
     turncount = turncount + 1
-    # PLAYER MOVE HERE
+    ask1 = input("Which space do you want to input for? (1-9)")
+    ask1 = is_space_taken(game_board, ask1)
+    dict[ask1] = "X"
     if dict["1"] == "X":
         dict["3"] = "O"
         print(gameboard(dict))
@@ -300,11 +396,14 @@ def top_middle_start(dict, turncount):
     else:
         print("you got an error in top_middle_start")
 
+
 def middle_left_start(dict, turncount):
     dict["5"] = "O"
     print(gameboard(dict))
     turncount = turncount + 1
-    # PLAYER MOVE HERE
+    ask1 = input("Which space do you want to input for? (1-9)")
+    ask1 = is_space_taken(game_board, ask1)
+    dict[ask1] = "X"
     if dict["1"] == "X":
         dict["7"] = "O"
         print(gameboard(dict))
@@ -341,11 +440,14 @@ def middle_left_start(dict, turncount):
         turncount = turncount + 1
         return turncount
 
+
 def middle_right_start(dict, turncount):
     dict["5"] = "O"
     print(gameboard(dict))
     turncount = turncount + 1
-    # PLAYER MOVE HERE
+    ask1 = input("Which space do you want to input for? (1-9)")
+    ask1 = is_space_taken(game_board, ask1)
+    dict[ask1] = "X"
     if dict["1"] == "X":
         dict["2"] = "O"
         print(gameboard(dict))
@@ -385,12 +487,13 @@ def middle_right_start(dict, turncount):
         print("you got an error in middle_right_start")
 
 
-
 def bottom_middle_start(dict, turncount):
     dict["5"] = "O"
     print(gameboard(dict))
     turncount = turncount + 1
-    # PLAYER MOVE HERE
+    ask1 = input("Which space do you want to input for? (1-9)")
+    ask1 = is_space_taken(game_board, ask1)
+    dict[ask1] = "X"
     if dict["1"] == "X":
         dict["4"] = "O"
         print(gameboard(dict))
@@ -430,10 +533,84 @@ def bottom_middle_start(dict, turncount):
         "You got an error in bottom_middle_start"
 
 
-def gameboard(game_board): #DO NOT CHANGE
-    print (" ", game_board["1"], " ", "|", " ", game_board["2"], " ", "|", " ", game_board["3"], " ",)
-    print("------|-------|------")
-    print (" ", game_board["4"], " ", "|", " ", game_board["5"], " ", "|", " ", game_board["6"], " ",)
-    print("------|-------|------")
-    print (" ", game_board["7"], " ", "|", " ", game_board["8"], " ", "|", " ", game_board["9"], " ",)
-    pass
+
+
+
+
+
+
+
+
+
+
+
+turncount = 0
+print(gameboard(game_board))
+win = ""
+for i in range(9):
+    turncount = gameplay(game_board, turncount)
+    if game_board["1"] == "X" and game_board["2"] == "X" and game_board["3"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["4"] == "X" and game_board["5"] == "X" and game_board["6"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["7"] == "X" and game_board["8"] == "X" and game_board["9"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["1"] == "X" and game_board["4"] == "X" and game_board["7"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["2"] == "X" and game_board["5"] == "X" and game_board["8"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["3"] == "X" and game_board["6"] == "X" and game_board["9"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["1"] == "X" and game_board["5"] == "X" and game_board["9"] == "X":
+        print("X wins!")
+        win = "yes"
+        break
+    elif game_board["7"] == "X" and game_board["5"] == "X" and game_board["3"] == "X":
+        print("X wins!")
+        break
+    elif game_board["1"] == "O" and game_board["2"] == "O" and game_board["3"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["4"] == "O" and game_board["5"] == "O" and game_board["6"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["7"] == "O" and game_board["8"] == "O" and game_board["9"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["1"] == "O" and game_board["4"] == "O" and game_board["7"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["2"] == "O" and game_board["5"] == "O" and game_board["8"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["3"] == "O" and game_board["6"] == "O" and game_board["9"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["1"] == "O" and game_board["5"] == "O" and game_board["9"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+    elif game_board["7"] == "O" and game_board["5"] == "O" and game_board["3"] == "O":
+        print("O wins!")
+        win = "yes"
+        break
+if win != "yes":
+    print("It's a tie!")
